7fb4ae3f634b3a387ec078b9ea43941b87208458
Vladislav Dolganov <v.dolganov@dunice.net>
2022-12-05 10:34:57 +0300

    fix reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 
 import newsReducer from './newsReducer';
 import authReducer from './authReducer';
-import modalReducer from './modalReducer';
 import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   news: newsReducer,
   auth: authReducer,
-  modal: modalReducer,
   user: userReducer,
 });
 
